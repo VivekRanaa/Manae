80ac98f38aab46836c4295b1bb014d4598994410
Vivekranaa <vrana3325@gmail.com>
2023-11-07 21:42:48 +0530

    final

---
+++ body.php
@@ -0,0 +1,80 @@
+
+   <div class="main main-raised">
+        <div class="container mainn-raised" style="width:100%;">
+  
+  <div id="myCarousel" class="carousel slide" data-ride="carousel">
+    <!-- Indicators -->
+   
+
+
+
+
+
+		<!-- SECTION -->
+		<div class="section mainn mainn-raised">
+		
+		
+			<!-- container -->
+			<div class="container">
+			
+				<!-- row -->
+				<div class="row">
+					<!-- shop -->
+					<div class="col-md-4 col-xs-6">
+						<a href="product.php?p=15"><div class="shop">
+							<div class="shop-img">
+								<img src="./img/vinyl-flooring.JPG" alt="">
+							</div>
+							<div class="shop-body">
+								<h3>FLOORING</h3>
+								<a href="product.php?p=15" class="cta-btn">Shop now <i class="fa fa-arrow-circle-right"></i></a>
+							</div>	
+						</div></a>
+					</div>
+					<!-- /shop -->
+
+					<!-- shop -->
+					<div class="col-md-4 col-xs-6">
+						<a href="product.php?p=1"><div class="shop">
+							<div class="shop-img">
+								<img src="./img/LAMINATES.jpeg" alt="">
+							</div>
+							<div class="shop-body">
+								<h3>LAMINATES</h3>
+								<a href="product.php?p=1" class="cta-btn">Shop now <i class="fa fa-arrow-circle-right"></i></a>
+							</div>
+						</div></a>
+					</div>
+					<!-- /shop -->
+
+					<!-- shop -->
+					<div class="col-md-4 col-xs-6">
+						<a href="product.php?p=23"><div class="shop">
+							<div class="shop-img">
+								<img src="./img/bricks.jpg" alt="">
+							</div>
+							<div class="shop-body">
+								<h3>STRUCTURE</h3>
+								<a href="product.php?p=23" class="cta-btn">Shop now <i class="fa fa-arrow-circle-right"></i></a>
+							</div>
+                            </div></a>
+					</div>
+					<!-- /shop -->
+				</div>
+				<!-- /row -->
+			</div>		
+			<!-- /container -->
+		</div>
+		<!-- /SECTION -->
+		  
+		
+
+
+									
+		
+					
+
+
+	
+										<!-- product -->
+				
